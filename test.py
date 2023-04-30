e47f54ee52ef0d8f186f7617dce84ea806a93dcb
Наиль <fano2458@gmail.com>
2023-04-30 18:07:55 +0600

    lines

--- test.py
+++ test.py
@@ -1,11 +1,34 @@
 import cv2
 import numpy as np
 from ultralytics import YOLO
+from utils import draw_line, writes_area_text, which_area
+
+"""
+TODO 
+1 split frame into sectors
+2 find to which sector does the given object belong
+"""
 
 model = YOLO('yolov8m.pt')
 
 video_path = "data/test.mp4"
 cap = cv2.VideoCapture(video_path)
+
+
+def draw_everything():
+    draw_line(annotated_frame, 0.00, 0.20, 0.10, 0.20) # Top-left line
+    draw_line(annotated_frame, 0.10, 0.25, 0.55, 0.05) # Top-middle line
+    draw_line(annotated_frame, 0.10, 0.25, 0.30, 0.80) # Left line
+    draw_line(annotated_frame, 0.35, 0.15, 0.65, 0.45) # Middle Line
+    draw_line(annotated_frame, 0.30, 0.80, 0.85, 0.25) # Bottom line
+    draw_line(annotated_frame, 0.55, 0.05, 0.85, 0.25) # Right line
+
+    writes_area_text(annotated_frame, "Register", 0.01, 0.25)
+    writes_area_text(annotated_frame, "Area 2 (A2)", 0.20, 0.05)
+    writes_area_text(annotated_frame, "Area 3 (A3)", 0.30, 0.40)
+    writes_area_text(annotated_frame, "Entrance", 0.70, 0.80)
+    writes_area_text(annotated_frame, "Area 1 (A1)", 0.60, 0.20)
+
 
 while cap.isOpened():
     success, frame = cap.read()
@@ -15,13 +38,14 @@ while cap.isOpened():
 
     results = model.predict(frame, save=False, imgsz=640, conf=0.35, classes=0)
     annotated_frame = results[0].plot()
-    
     boxes = results[0].boxes
-    points = []
+    
     for box in boxes:
         x1,y1,x2,y2 = box.xyxy[0].detach().cpu()
         x, y = x2-(x2-x1)/2, y2-(y2-y1)/2
         cv2.circle(annotated_frame,(int(x),int(y)),3,(255,0,0),3)
+
+    draw_everything()
 
     cv2.imshow("YOLOv8 Inference", annotated_frame)
 
