f65fb360e1c14618b923a1b046ef1be8d0432718
Наиль <fano2458@gmail.com>
2023-06-13 17:59:41 +0600

    time_spent

--- test.py
+++ test.py
@@ -39,6 +39,7 @@ def draw_everything():
 prev_frame_time = 0
 next_frame_time = 0
 areas_names = ['Register','Entrance','A1','A2','A3']
+time_spent = {}
 
 f = open('values.csv','w',newline='')
 writer = csv.DictWriter(f,fieldnames=areas_names)
@@ -50,10 +51,11 @@ while cap.isOpened():
     if not success:
         break
     #TODO change
-    results = model.predict(frame, show_labels=False, show_conf=False, 
-        save=False, imgsz=640, conf=0.35, classes=0,augment=False)
+    results = model.predict(frame, stream=False, show_labels=True, show_conf=False, 
+        save=False, imgsz=640, conf=0.35, classes=0)
     
-    annotated_frame = results[0].plot()
+    #annotated_frame = results[0].plot()
+    annotated_frame = frame
     boxes = results[0].boxes
     areas = {'Register':0,'Entrance':0,'A1':0,'A2':0,'A3':0}
     boxes_d = boxes.xywh
@@ -76,7 +78,8 @@ while cap.isOpened():
                     1,(0,255,0),1,cv2.LINE_AA)
 
     outputs = deepsort.update(boxes_d,confs_d,indic_d,frame)
-    
+    inds = []
+
     if len(outputs) > 0:
         boxes = outputs[:, :4]
         object_id = outputs[:, -1]
@@ -87,6 +90,7 @@ while cap.isOpened():
             x1,y1,x2,y2 = list(map(int, box))
             x, y = x2-(x2-x1)/2, y2-(y2-y1)/2
             index = int(indentities[i]) if indentities is not None else 0
+            inds.append(index)
 
             text = f'ID:{str(index)}'
             cv2.putText(annotated_frame,text,(int(x),int(y+10)),0,1,(0,255,255),2,lineType=cv2.FILLED)
@@ -94,6 +98,12 @@ while cap.isOpened():
     writer.writerow(areas)
 
     next_frame_time = time.time()
+
+    for ind in inds:
+        try:
+            time_spent[str(ind)] += next_frame_time-prev_frame_time
+        except KeyError:
+            time_spent[str(ind)] = 0
     try:
         fps = str(round(1/(next_frame_time-prev_frame_time),2))
     except ZeroDivisionError:
@@ -101,7 +111,7 @@ while cap.isOpened():
     prev_frame_time = next_frame_time
 
     cv2.putText(annotated_frame,"FPS: "+fps,(7,30),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),3,cv2.LINE_AA)
-    cv2.imshow("YOLOv8 Inference", annotated_frame)
+    cv2.imshow("Tracking", annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
@@ -109,3 +119,5 @@ while cap.isOpened():
 f.close()
 cap.release()
 cv2.destroyAllWindows()
+
+print(time_spent)
